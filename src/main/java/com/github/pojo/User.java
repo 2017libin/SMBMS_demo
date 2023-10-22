1da4e52988d6402e4b1d0f0dad4176e971029462
2017chase <1963482624@qq.com>
2023-10-22 17:02:06 +0800

    first commit

---
+++ src/main/java/com/github/pojo/User.java
@@ -0,0 +1,33 @@
+package com.github.pojo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @Author libin
+ * @Date 2023/10/22
+ */
+@Data  // 通过Data注解来自动生成属性的getter/setter、toString方法
+public class User {
+    private Integer id;
+    private String userCode;
+    private String userName;
+    private String userPassword;
+    private Integer gender;
+    private Date birthday;
+    private String phone;
+    private String address;
+    private Integer userRole;
+    private Integer createdBy;
+    private Date createdDate;
+    private Integer modifyBy;
+    private Date modifyDate;
+    private Integer age;
+    private String userRoleName;
+    public Integer getAge(){
+        Date date = new Date();
+        Integer age = date.getYear() - birthday.getYear();
+        return age;
+    }
+}
