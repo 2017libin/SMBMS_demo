f1438b3b621e82ce2760e386e0e0f9f220bcd2e3
2017chase <1963482624@qq.com>
2023-10-30 16:27:04 +0800

    add filter for CharacterEncoding

---
+++ src/main/java/com/github/filter/CharacterEncodingFilter.java
@@ -0,0 +1,23 @@
+package com.github.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+// 设置请求和响应的字符编码
+public class CharacterEncodingFilter implements Filter {
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        // 传递给下一个过滤器
+        chain.doFilter(request,response);
+    }
+
+    public void destroy() {
+
+    }
+}
