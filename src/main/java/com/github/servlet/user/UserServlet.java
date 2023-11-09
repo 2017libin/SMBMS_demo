ecf7eb434ee8f63c8bbc1a65e39be91cee22ab99
2017chase <1963482624@qq.com>
2023-11-10 01:18:17 +0800

    add query for UserServlet

--- src/main/java/com/github/servlet/user/UserServlet.java
+++ src/main/java/com/github/servlet/user/UserServlet.java
@@ -1,10 +1,14 @@
 package com.github.servlet.user;
 
 import com.alibaba.fastjson.JSONArray;
+import com.github.pojo.Role;
 import com.github.pojo.User;
+import com.github.service.role.RoleService;
+import com.github.service.role.RoleServiceImpl;
 import com.github.service.user.UserService;
 import com.github.service.user.UserServiceImpl;
 import com.github.util.Constants;
+import com.github.util.PageSupport;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class UserServlet extends HttpServlet {
@@ -25,6 +30,8 @@ public class UserServlet extends HttpServlet {
             this.updatePwd(request, response);
         } else if(method.equals("pwdmodify") && method!=null){
             this.pwdModify(request,response);
+        } else if(method != null && method.equals("query")) {
+            this.query(request,response);
         }
     }
 
@@ -38,6 +45,8 @@ public class UserServlet extends HttpServlet {
                 this.updatePwd(request, response);
             } else if(method.equals("pwdmodify")){  // 验证旧密码是否正确
                 this.pwdModify(request,response);
+            } else if(method != null && method.equals("query")) {
+                this.query(request,response);
             }
         }
     }
@@ -101,4 +110,76 @@ public class UserServlet extends HttpServlet {
 
     }
 
+    // 用户管理
+    public void query(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // 查询用户列表
+        String queryUserName = request.getParameter("queryname");
+        String temp = request.getParameter("queryUserRole");
+        String pageIndex = request.getParameter("pageIndex");
+        int queryUserRole = 0;
+        UserService userService = new UserServiceImpl();
+        List<User> userList = null;
+
+        // 设置页面容量
+        int pageSize = Constants.pageSize;
+
+        // 当前页码
+        int currentPageNo = 1;
+        /**
+         * http://localhost:8090/SMBMS/userlist.do
+         * ----queryUserName --NULL
+         * http://localhost:8090/SMBMS/userlist.do?queryname=
+         * --queryUserName ---""
+         */
+        System.out.println("queryUserName servlet-------->"+queryUserName);
+        System.out.println("queryUserRole servlet-------->"+queryUserRole);
+        System.out.println("query pageIndex----------> " + pageIndex);
+
+        if(queryUserName == null){
+            queryUserName = "";
+        }
+        if(temp != null && !temp.equals("")){
+            queryUserRole = Integer.parseInt(temp);
+        }
+
+        if(pageIndex != null){
+            try{
+                currentPageNo = Integer.parseInt(pageIndex);
+            }catch(NumberFormatException e){
+                response.sendRedirect("error.jsp");
+            }
+        }
+        // 满足条件的数据数量
+        int totalCount	= userService.getUserCount(queryUserName, queryUserRole);
+
+        // 总页数
+        PageSupport pages = new PageSupport();
+        pages.setCurrentPageNo(currentPageNo);
+        pages.setPageSize(pageSize);
+        pages.setTotalCount(totalCount);  // 数据的总数
+
+        int totalPageCount = pages.getTotalPageCount();
+
+        // 控制首页和尾页
+        if(currentPageNo < 1){
+            currentPageNo = 1;
+        }else if(currentPageNo > totalPageCount){
+            currentPageNo = totalPageCount;
+        }
+
+
+        userList = userService.getUserList(queryUserName,queryUserRole,currentPageNo, pageSize);
+        request.setAttribute("userList", userList);
+        List<Role> roleList = null;
+        RoleService roleService = new RoleServiceImpl();
+        roleList = roleService.getRoleList();
+        request.setAttribute("roleList", roleList);
+        request.setAttribute("queryUserName", queryUserName);
+        request.setAttribute("queryUserRole", queryUserRole);
+        request.setAttribute("totalPageCount", totalPageCount);
+        request.setAttribute("totalCount", totalCount);
+        request.setAttribute("currentPageNo", currentPageNo);
+        request.getRequestDispatcher("userlist.jsp").forward(request, response);
+    }
 }
